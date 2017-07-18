d869d5a2defc98a40fe0a1651893f84dd4bfef7b
root <adres199219@gmail.com>
2017-07-18 19:02:50 +0300

    First Class

---
+++ app/Page.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Page extends Model
+{
+    //
+    protected  $table="pages";
+
+    protected $fillable=[
+        'name',
+        'alias',
+        'img',
+        'text',
+        'create_at',
+        'update_at'
+    ];
+}
